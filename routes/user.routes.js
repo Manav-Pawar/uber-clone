9bfb44769f37c3e58a7a20f997a35e039cda749b
Manav-Pawar <pawar.manav2304@gmail.com>
2025-04-11 17:37:07 +0530

    user authentication complete

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { body } = require("express-validator");
 const userController = require ('../controllers/user.controller')
+const authMiddleware = require('../middleware/auth.middelware')
 
 const router = express.Router();
 router.post("/register",[
@@ -16,5 +17,9 @@ router.post("/login",[
 ],
 userController.loginUser
 )
+
+router.get('/profile',authMiddleware.authUser,userController.getUserProfile)
+
+router.get('/logout',authMiddleware.authUser,userController.logoutUser)
 
 module.exports = router;
